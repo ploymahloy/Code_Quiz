046992c64c57e94d07a42c73a1a22e90774752bb
Patrick Mahloy <PatrickMahloy@Patricks-MBP.fios-router.home>
2020-06-23 12:27:00 -0400

    tutor helped with pseudo-coding

---
+++ script.js
@@ -0,0 +1,59 @@
+// write questions (array)
+
+var questions = [
+    {
+        ques: "test question",
+        choices: ["1", "2", "3", "4"],
+        answer: "2"
+    },
+    {
+
+    }
+]
+
+// declare vars to keep track of the quiz
+     // var indexOfQues. set to 0
+     // var time = questions.length * 15
+     // var timer;
+
+
+// declare vars to grab the elements from html file
+    // var quesElement = document.
+
+// function to start the quiz ---> line 30-35
+// function to get questions
+// function to check if the user guesses right or wrong
+// function to deduct time if answer is wrong 
+// function for the quiz to end
+// function to update time
+// function to save score and initials
+
+// declar var to start screen
+// doc.elbyID
+// startScreen.setAttribute("class", "hide")
+// quesEl.removeAttribute("class")
+// timer = setInterval(functionForTime, 1000)
+// .textContent = function(getQuestions)
+     // appends html 
+
+// getquestion function
+// get current question from the array
+//clear out any questions choices
+// loop over the choices 
+// create an event listener to user choice
+// display on page 
+// function (var i<0; i<questions.length; i++)
+// function to deduct time
+// var userChoice
+
+    var userChoice = //html btns
+
+    if (userChoice !== answers) {
+        time -= 5;
+    }
+    else 
+
+// time--
+//.textContent to show it on page 
+// check if time ran out
+// ends quiz
