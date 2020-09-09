41fa00b867d225b714e3d00ec09455e98bd807ea
Patrick Mahloy <PatrickMahloy@Patricks-MBP.fios-router.home>
2020-09-08 21:50:29 -0400

    got help from Ty on the quiz functions, need to add highscore portion of assignment

--- script.js
+++ script.js
@@ -17,70 +17,105 @@ var questions = [
         answer: "<script>"
     },
     {
-        question: "What is the proper file extension for external JavaScript files?",
+        question: "What is the proper file extension for JavaScript files?",
         choices: [".js", ".script", ".jvs", ".mocha"],
         answer: ".js"
     },
     {
         question: "Which of the following properly declares a variable?",
-        choices: ["I declare the following a variable: kingArthurVar", "Issa variable = 21SavageVar", "variable = issaVar", "var = camelCase"],
+        choices: ["I declare the following a variable: kingArthurVar", 
+                  "Issa variable = 21SavageVar", 
+                  "variable = issaVar", 
+                  "var = camelCase"],
         answer: "var = camelCase"
     },
-]
+];
 
 // declare vars to keep track of the quiz
-     var indexOfQuestions = 0;
-     var time = questions.length * 20;
-     var timer = document.getElementById("Timer");
-
+     
+    var indexOfQuestions = 0;
+    var time = questions.length * 20;
+    var timer = document.getElementById("Timer");
+    var startBtn = document.getElementById("start-btn");
+    console.log(startBtn);
 
 // declare vars to grab the elements from html file
+    
     var quesEl = document.getElementById("Question");
     var ansEl = document.getElementById("Answers");
-    var userChoice = ;
-    var output = [];
-    var answers;
+    var ans1 = document.getElementById("ans1");
+    var ans2 = document.getElementById("ans2");
+    var ans3 = document.getElementById("ans3");
+    var ans4 = document.getElementById("ans4");
+    var answers = [];
+    var hidden = document.getElementsByClassName("hide");
+    var quizTimer;
+
+// function to cycle through questions
+    
+    // userChoice.addEventListener("click", function () {
+    //     for (var i = 0; i < questions.length; i++) 
 
 
-// function to start the quiz ---> line 30-35
-// function to get questions
-// function to check if the user guesses right or wrong
-// function to deduct time if answer is wrong 
-// function for the quiz to end
-// function to update time
-    function timeFunction() {
+// function to start the quiz 
+function countdown() {
+    quizTimer = setInterval(function () {
         time--;
-        if (userChoice !== answers) {
-            time -= 5;
-        }
-        if ()
+        console.log(time);
+    }, 1000)
+};
+
+function getQuestions() {
+    var getQuestion = questions[indexOfQuestions];
+    quesEl.textContent = getQuestion.question; 
+    ansEl.innerHTML = "";
+    
+    getQuestion.choices.forEach(function(choice, index) {
+        var choiceBtn = document.createElement("button");
+        choiceBtn.setAttribute("class", "choice");
+        choiceBtn.setAttribute("value", choice);
+        choiceBtn.textContent = index + 1 + ") " + choice;
+        choiceBtn.onclick = choiceClick; 
+        ansEl.appendChild(choiceBtn);
+    });
+};
+
+function choiceClick() {
+    if (this.value !== questions[indexOfQuestions].answer) {
+        alert("FAILURE");
+        time -= 5;
     }
-// function to save score and initials
+    else {
+        alert("CORRECT");
+    }
+    indexOfQuestions++;
+    if (indexOfQuestions === questions.length) {
+        endQuiz();
+    }
+    else {
+        getQuestions();
+    }
+};
 
-// declare var to start screen
-    var startScreen;
-doc.elbyID
-startScreen.setAttribute("class", "hide")
-quesEl.removeAttribute("class")
-timer = setInterval(functionForTime, 1000)
-.textContent = function(getQuestions)
-     // appends html 
+function startQuiz() {
+    console.log("test");
+    countdown()
+    for (var i = 0; i < hidden.length; i += 1) {
+        hidden[i].style.display = 'block';
+    }
+    getQuestions();
+};
 
-// getquestion function
-// get current question from the array
-//clear out any questions choices
-// loop over the choices 
-// create an event listener to user choice
-// display on page 
-// function (var i<0; i<questions.length; i++)
-// function to deduct time
-// var userChoice
+startBtn.addEventListener("click", startQuiz);
 
-    var userChoice = //html btns
+// function for the quiz to end (out of time or questions)
 
+    function endQuiz () {
+        clearInterval(quizTimer);
+        document.getElementById("Card").innerHTML = "";
+        var finalScore = document.createElement("p")
+        finalScore.textContent = "Congrats!...your score is" + time;
+        startBtn.appendChild(finalScore);
+    };
 
-// time--
-//.textContent to show it on page 
-// check if time ran out
-    if 
-// ends quiz
+// add div for Response, avoid alerts
